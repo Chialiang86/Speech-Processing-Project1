0c4f631d4bd1cd9a1931e3a196429a21bf4adda6
Chialiang <a930039930039@gmail.com>
2020-04-11 00:40:00 +0800

    pitch starts

--- test_sound.py
+++ test_sound.py
@@ -1,4 +1,5 @@
 
+# In[1]
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,7 +8,8 @@ import librosa
 import librosa.display
 import IPython.display as ipd
 
-fname = 'Multimedia2.wav'
+fname = 'sound_src/Multimedia3.wav'
+version = 6
 
 wave, fs = librosa.load(fname)
 
@@ -16,7 +18,10 @@ ipd.Audio(fname) # load a local WAV file
 plt.figure(figsize=(25, 5))
 librosa.display.waveplot(wave, sr=fs)
 plt.title('waveform')
-plt.show()
+plt.savefig("output/Waveform_" + str(version) + ".png")
+#plt.show()
+
+# In[2]
 
 #zcr
 
@@ -29,21 +34,22 @@ def sgn(l):
 def zero_crossing_rate(wave, fsize, overlapfsize):
     wsize = fsize - overlapfsize
     testnum = ma.ceil(len(wave) / wsize)
-    zrc = np.zeros((testnum,1))
+    zcr = np.zeros((testnum,1))
     for i in range(testnum):
         testdata = wave[i * wsize : min(i * wsize + fsize, len(wave))]
         cache = abs(np.array(sgn(testdata[1::])) - np.array(sgn(testdata[0:-1]))) 
-        zrc[i] = sum(cache) / (2 * len(testdata))
-    return zrc
+        zcr[i] = sum(cache) / (2 * len(testdata))
+    return zcr
 
-zrc = zero_crossing_rate(wave, 256, 0)
-time = np.arange(0, len(zrc)) * (len(wave) / len(zrc) / fs)
+zcr = zero_crossing_rate(wave, 256, 0)
+time = np.arange(0, len(zcr)) * (len(wave) / len(zcr) / fs)
 plt.figure(figsize = (25,5))
-plt.plot(time, zrc)
+plt.plot(time, zcr)
 plt.xlabel('time (sec)')
 plt.ylabel('zero crossing rate')
-plt.title('ZRC')
-plt.show()
+plt.title('ZCR')
+plt.savefig("output/ZCR_" + str(version) + ".png")
+#plt.show()
 
 #short time energy
 def hamming_window(m, n, M):
@@ -63,17 +69,20 @@ def short_time_energy(wave, fsize, overlapfsize):
     for i in range(testnum):
         interval = np.arange(i * wsize , min(i * wsize + fsize, len(wave)))
         testdata = wave[i * wsize : min(i * wsize + fsize, len(wave))]
-        #ste[i] = sum( np.multiply( np.power(testdata, 2), np.power(hamming_window(interval, interval[0], len(testdata)), 2) )) 
+        ste[i] = sum( np.multiply( np.power(testdata, 2), np.power(hamming_window(interval, interval[0], len(testdata)), 2) )) 
         ste[i] = sum( np.power(testdata, 2))
     return ste
 
 ste = short_time_energy(wave, 256, 0)
-time = np.arange(0, len(zrc)) * (len(wave) / len(zrc) / fs)
+time = np.arange(0, len(zcr)) * (len(wave) / len(zcr) / fs)
 plt.figure(figsize = (25,5))
 plt.plot(time, ste)
 plt.xlabel('time (sec)')
 plt.ylabel('energy')
 plt.title('short time energy')
+plt.savefig("output/STE_" + str(version) + ".png")
+frame_idxs = np.where( ste > 0.5)[0]
+print(frame_idxs)
 plt.show()
 
 
